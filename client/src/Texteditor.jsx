b3bcafd18185824fcecfe31028c5c46b84615a96
Suhas H <156500336+suhasprof@users.noreply.github.com>
2024-07-20 17:32:36 +0530

    Update Texteditor.jsx

--- client/src/Texteditor.jsx
+++ client/src/Texteditor.jsx
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("http://localhost:3001");
+    const s = io("https://docs-server-ahtv.onrender.com");
     setSocket(s);
 
     return () => {
